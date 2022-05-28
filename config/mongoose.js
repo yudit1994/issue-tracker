7796f78de6c8048e6950b72c43e72d96c6f0b490
udit <yudit1994@gmail.com>
2022-05-28 21:36:27 +0530

    first

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const db= mongoose.connection;
 
 
-mongoose.connect('mongodb://localhost:27017/issue-tracker');
-
+mongoose.connect(process.env.db || 'mongodb://localhost:27017/issue-tracker');
 
 module.exports=db;
